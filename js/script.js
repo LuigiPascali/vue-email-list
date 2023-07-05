e263b7451db580e355235726413efc47bf7e874a
luigimariapascali <luigimariapascali@gmail.com>
2023-07-05 16:52:02 +0200

    bonus complete: esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,37 +1,79 @@
+// // Inizializzazione Vue Js:
+// const app = Vue.createApp({
+//   data() {
+//     return {
+//       emails: [],
+//     };
+//   },
+//   mounted() {
+//     this.generateEmails();
+//   },
+//   methods: {
+//     generateEmails() {
+//       for (let i = 0; i < 10; i++) {
+//         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+//           .then(response => {
+//             this.emails.push(response.data.response);
+
+//             console.log("Email generata:", response.data.response);
+
+//             if (this.emails.length === 10) {
+
+//               console.log("Tutte le email sono state generate:", this.emails);
+
+//             }
+//           })
+//           .catch(error => {
+
+//             console.error("Errore nella richiesta:", error);
+
+//           });
+//       }
+//     },
+//   },
+// });
+
+// // Mostra l'applicazione nella pagina HTML
+// app.mount('#app');
+
+
+// BONUS: 
+
 // Inizializzazione Vue Js:
 const app = Vue.createApp({
   data() {
     return {
       emails: [],
+      allEmailsGenerated: false,
+      generatingEmails: false,
     };
-  },
-  mounted() {
-    this.generateEmails();
   },
   methods: {
     generateEmails() {
+      this.generatingEmails = true;
+      const requests = [];
+
       for (let i = 0; i < 10; i++) {
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-          .then(response => {
-            this.emails.push(response.data.response);
-
-            console.log("Email generata:", response.data.response);
-
-            if (this.emails.length === 10) {
-
-              console.log("Tutte le email sono state generate:", this.emails);
-
-            }
-          })
-          .catch(error => {
-
-            console.error("Errore nella richiesta:", error);
-            
-          });
+        requests.push(
+          axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        );
       }
+
+      axios.all(requests)
+        .then(responses => {
+          this.emails = responses.map(response => response.data.response);
+          this.allEmailsGenerated = true;
+          this.generatingEmails = false;
+          console.log("Tutte le email sono state generate:", this.emails);
+        })
+        .catch(error => {
+          console.error("Errore nella richiesta:", error);
+          this.generatingEmails = false;
+        });
     },
   },
 });
 
-  // Mostra l'applicazione nella pagina HTML
-  app.mount('#app');
+
+// Mostra l'applicazione nella pagina HTML
+app.mount('#app');
