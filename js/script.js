a96280a53100e889f7fc1c7bb936d6d62515ffbb
luigimariapascali <luigimariapascali@gmail.com>
2023-07-05 16:03:35 +0200

    Milestone complete

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
 // Inizializzazione Vue Js:
 const app = Vue.createApp({
-    data() {
-      return {
-        
-      };
-    }
-  });
+  data() {
+    return {
+      emails: [],
+    };
+  },
+  mounted() {
+    this.generateEmails();
+  },
+  methods: {
+    generateEmails() {
+      for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then(response => {
+            this.emails.push(response.data.response);
+
+            console.log("Email generata:", response.data.response);
+
+            if (this.emails.length === 10) {
+
+              console.log("Tutte le email sono state generate:", this.emails);
+
+            }
+          })
+          .catch(error => {
+
+            console.error("Errore nella richiesta:", error);
+            
+          });
+      }
+    },
+  },
+});
 
   // Mostra l'applicazione nella pagina HTML
   app.mount('#app');
